3de65ec598c794b9a775c1c4d699cc90c8e0f57b
mandooism <mandoo.cache@gmail.com>
2025-10-12 17:30:39 +0900

    refactor: 토픽 상태를 App.jsx 에서 관리하도록 함 - before: TopicBox.jsx 에서 관리하고 있었음

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import TopicBox from "./components/TopicBox";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./reset.css";
 import "./App.css";
 
+const randomFrom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
 function App() {
    const [time, setTime] = useState(20); // 분 단위
+   const [topic, setTopic] = useState(null);
+   const [bgUrl, setBgUrl] = useState("");
+
+   const loadRandomTopic = async () => {
+      const res = await fetch("/topics.json");
+      const data = await res.json();
+      setTopic(randomFrom(data));
+   };
+
+   useEffect(() => {
+      loadRandomTopic();
+   }, []);
+
+   useEffect(() => {
+       console.log(topic &&  `/assets/backgrounds/${topic.eng}.jpg`)
+
+      setBgUrl(
+         topic
+            ? `/assets/backgrounds/${topic.eng}.jpg`
+            : "/assets/backgrounds/default.jpg",
+      );
+   }, [topic]);
 
    return (
-      <div className="app">
-         <TopicBox />
-
+      <div
+         className="app"
+         style={{
+            backgroundImage: `url(${bgUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            transition: "background-image 0.3s ease-in-out",
+         }}
+      >
+         {" "}
+         <TopicBox onTopicClick={loadRandomTopic} topic={topic} />
          <div className="timer">{String(time).padStart(2, "0")}:00</div>
-
          <div className="time-buttons">
             <button onClick={() => setTime(10)}>10분</button>
             <button onClick={() => setTime(20)}>20분</button>
--- src/components/TopicBox.jsx
+++ src/components/TopicBox.jsx
@@ -1,29 +1,11 @@
-import { useEffect, useState } from "react";
-
-export default function TopicBox() {
-   const [topics, setTopics] = useState([]);
-   const [current, setCurrent] = useState(null);
-
-   useEffect(() => {
-      fetch("/topics.json")
-         .then((res) => res.json())
-         .then((data) => {
-            setTopics(data);
-            setCurrent(randomFrom(data));
-         });
-   }, []);
-
-   const randomFrom = (arr) => arr[Math.floor(Math.random() * arr.length)];
-
-   const handleClick = () => {
-      if (topics.length > 0) {
-         setCurrent(randomFrom(topics));
-      }
-   };
-
+export default function TopicBox({ topic, onTopicClick }) {
    return (
-      <h1 className="topic" onClick={handleClick} style={{ cursor: "pointer" }}>
-         {current ? current.kor : "불러오는 중..."}
+      <h1
+         className="topic"
+         style={{ cursor: "pointer" }}
+         onClick={onTopicClick}
+      >
+         {topic ? topic.kor : "불러오는 중..."}
       </h1>
    );
 }
