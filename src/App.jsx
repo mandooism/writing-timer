3c8162ecc388388c273ac9e033a1782f5da7dc5f
mandooism <mandoo.cache@gmail.com>
2025-11-29 18:17:22 +0900

    fix: next -> react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { useState, useEffect } from "react";
 import TopicBox from "./components/TopicBox";
 import Timer from "./components/Timer";
